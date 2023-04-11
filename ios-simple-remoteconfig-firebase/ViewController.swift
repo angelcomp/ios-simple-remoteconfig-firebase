fa907e0f678fe55445b38a1a3624ec858bbea994
Angelica dos Santos <santosangelicassp@gmail.com>
2023-04-11 08:37:23 -0300

    Initial Commit

---
+++ ios-simple-remoteconfig-firebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-simple-remoteconfig-firebase
+//
+//  Created by Angelica dos Santos on 11/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
