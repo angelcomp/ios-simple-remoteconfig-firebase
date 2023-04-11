6511d00c1815696310626fe3334dd9bc6dfe559a
Angelica dos Santos <santosangelicassp@gmail.com>
2023-04-11 10:26:54 -0300

    boolean remote config

--- ios-simple-remoteconfig-firebase/ViewController.swift
+++ ios-simple-remoteconfig-firebase/ViewController.swift
@@ -6,14 +6,86 @@
 //
 
 import UIKit
+import FirebaseRemoteConfig
 
 class ViewController: UIViewController {
 
+    // MARK: private properties
+    private let purpleView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .purple
+        view.isHidden = true
+        return view
+    }()
+    
+    private let blueView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .blue
+        view.isHidden = true
+        return view
+    }()
+    
+    private let remoteConfig = RemoteConfig.remoteConfig()
+    
+    // MARK: override funcs
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(purpleView)
+        view.addSubview(blueView)
+        
+        fetchValues()
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        purpleView.frame = view.bounds
+        blueView.frame = view.bounds
+    }
+    
+    // MARK: private funcs
+    
+    private func fetchValues() {
+        //toggle_ui_background
+        
+        let defaults: [String: NSObject] = [
+            "toggle_ui_background" : false as NSObject
+        ]
+        
+        remoteConfig.setDefaults(defaults)
+        
+        let settings = RemoteConfigSettings()
+        settings.minimumFetchInterval = 0
+        remoteConfig.configSettings = settings
+        
+        let cachedValue = self.remoteConfig.configValue(forKey: "toggle_ui_background").boolValue
+        updateUI(showPurpleBackground: cachedValue)
+        
+        self.remoteConfig.fetch(withExpirationDuration: 0, completionHandler: { status, error in
+            if status == .success, error == nil {
+                
+                self.remoteConfig.activate(completion: { _, error in
+                    guard error == nil else { return }
+                    
+                    let value = self.remoteConfig.configValue(forKey: "toggle_ui_background").boolValue
+                    
+                    print("value: \(value)")
+                    
+                    DispatchQueue.main.async {
+                        self.updateUI(showPurpleBackground: value)
+                    }
+                })
+            } else {
+                print("something went wrong")
+            }
+        })
+    }
+    
+    private func updateUI(showPurpleBackground: Bool) {
+        purpleView.isHidden = !showPurpleBackground
+        blueView.isHidden = showPurpleBackground
+    }
 }
 
